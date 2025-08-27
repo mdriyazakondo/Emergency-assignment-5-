183bffa4758f1262a08f3c6d3b314aa3a3023e87
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-08-27 11:47:38 +0600

    added degien section

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const heart = document.getElementById("heart");
+const coine = document.getElementById("coine");
+const copy = document.getElementById("copy");
+const copyBtn = document.getElementById("copyBtn");
+const callBtn = document.getElementById("callBtn");
